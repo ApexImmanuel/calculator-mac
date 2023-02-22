95085f38a9cd76cd5d85ee49d5e3183aebac4847
ApexImmanuel <apexbecham@hotmail.com>
2023-02-22 18:44:29 +0100

    Equal Buutton Sums Up The Value

--- README.md
+++ README.md
@@ -13,3 +13,21 @@ minus function ();
 divide function ();
 times function ();
 
+
+JS pseudo/
+operator on click|
+currentvalue should be empty
+previousValue should take the value of the currentValue;
+screen should show the previousValue and the operator
+
+
+onClick previousValue = currentValue;
+onClick currentValue = "";
+currentValue + previousValue is will be stored in result;
+The result needs to keep adding up;
+for example 1 + 1 = 2; +4 = 6;
+result keeps adding up the previousValue;
+
+
+ 
+
--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-let previouseValue = "";
+let previousValue = "";
 let currentValue = "";
+let calOperator  = "";
+// alert("Instruction: To use this calculator you must press the '=' operator to keep adding numbers. For Example instead of pressing 4+4+5. press 4+4 = 'answer' + 5");
 
 const clear = document.querySelector("#clear");
 const deleteButton = document.querySelector("#delete");
@@ -14,12 +16,57 @@ numbers.forEach(number => {
     number.addEventListener("click", (e) => {
         calNumber(e.target.defaultValue);
         currentScreen.innerText = currentValue;
-
-    })
-})
-
+    });
+});
 function calNumber (num) {
-    currentValue += num;
-    console.log(num);
+    if (currentValue.length <= 11) {
+        currentValue += num;
+    }
 }
 
+operators.forEach(operator => {
+    operator.addEventListener("click", (e) => {
+        operate(e.target.defaultValue);
+        previousValue  = currentValue;
+        currentValue = "";
+        currentScreen.innerText = "";
+        previousScreen.innerText = `${previousValue} ${calOperator}`;
+    });
+});
+function operate (op) {
+    calOperator = op; 
+}
+
+
+equal.addEventListener("click", calculate)
+function calculate () {
+    previousValue = +previousValue;
+    currentValue = +currentValue;
+    if (calOperator === "+") {
+        previousScreen.innerText = `${previousValue} ${calOperator} ${currentValue}`;
+        previousValue  = previousValue + currentValue;
+    } else if (calOperator === "-") {
+        previousScreen.innerText = `${previousValue} ${calOperator} ${currentValue}`;
+        previousValue = previousValue - currentValue;
+    } else if (calOperator === "x") {
+        previousScreen.innerText = `${previousValue} ${calOperator} ${currentValue}`;
+        previousValue = parseInt(previousValue) * parseInt(currentValue);
+    } else if (calOperator === "÷") {
+        previousScreen.innerText = `${previousValue} ${calOperator} ${currentValue}`;
+        previousValue = previousValue / currentValue;
+    }
+    currentValue = previousValue.toString();
+    currentScreen.innerText = currentValue;
+}
+
+clear.addEventListener("click", () => {
+    previousScreen.innerText = "";
+    currentScreen.innerText = "";
+    previousValue = "";
+    currentValue = "";
+    result = "";
+});
+deleteButton.addEventListener("click", () => {
+    currentValue = currentValue.substring(0, currentValue.length-1);
+    currentScreen.innerText = currentValue;
+});
